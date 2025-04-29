b2576573c61c3d8ce45bd8d626caf72a72f5ca82
gregor-J8 <gregor.johnson@outlook.com>
2025-04-29 13:37:52 -0500

    updated endpoint for facility minerals

--- obj/Debug/net8.0/ExomineAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExomineAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExomineAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f71741dc0ae0fbba6b8c2bb0a518b14be0bf51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a3bd5927c5209f7fd935a0a1dbf18aedf0bfbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExomineAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExomineAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
